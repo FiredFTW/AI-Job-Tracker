a6339f4be2acc8f81f916f891a91653dec114494
Dan <ZLAC211@live.rhul.ac.uk>
2025-06-11 18:44:53 +0100

    feat: update API base URL to use environment variable and add CORS configuration

--- server/index.js
+++ server/index.js
@@ -11,6 +11,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://lifedash-server.onrender.com' 
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 // Middleware
 app.use(cors()); // Allows requests from other origins 
 app.use(express.json()); // Allows us to parse JSON in the request body
