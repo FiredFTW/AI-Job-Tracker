58dd10da58d019da6536c38a1f22737ce3d7135f
FiredFTW <banthorpedaniel@gmail.com>
2025-06-11 23:05:14 +0100

    fix: incorrect URL in variable

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173', 
-  'https://lifedash-server.onrender.com' 
+  'https://life-dash-psi.vercel.app' 
 ];
 
 const corsOptions = {
